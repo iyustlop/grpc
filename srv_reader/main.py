56353dd0fdcd1ecff284733f0b7047b8c20bd71e
iyustlop <iyustlop@gmail.com>
2022-03-31 16:25:36 +0200

    Opem a file and deliver messages

--- srv_reader/main.py
+++ srv_reader/main.py
@@ -1,15 +1,18 @@
-from urllib import request
 import sales_records_pb2
 import sales_records_pb2_grpc
 import grpc
-
+from repositories.file_repository import FileRepository
 
 def main():
-    with grpc.insecure_channel('srv_persistor:50051') as channel:
-        stub = sales_records_pb2_grpc.SalesRecordsStub(channel)
-        request = sales_records_pb2.EmptyMesage()
-        result = stub.PingSalesRecords(request)
-        print(f"GRPC received: {result.ack}")
+    file_repository = FileRepository('/tmp/data/100_sr.csv')
+    data_readed = file_repository.read_data()
+
+    for row in data_readed:
+        with grpc.insecure_channel('srv_persistor:50051') as channel:
+            stub = sales_records_pb2_grpc.SalesRecordsStub(channel)
+            request = sales_records_pb2.EmptyMesage()
+            result = stub.PingSalesRecords(request)
+            print(f"GRPC received: {result.ack}")
 
 
 if __name__ == "__main__":
